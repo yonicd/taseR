d3ee1cd43434d18c5d0806a8b2d3271b9007e6fa
yonicd <yonicd@gmail.com>
2015-07-07 06:33:03 +0300

    update tase security

--- tase_security_selenium.r
+++ tase_security_selenium.r
@@ -17,46 +17,45 @@ RSelenium::startServer()
 remDr <- remoteDriver()
 remDr$open(silent = F)
 
-df.in=stockID%>%filter(Name%in%c("TEVA"))
+df.in=stockID%>%filter(Name%in%c("TEVA","LEUMI"))
 df.in$from.date=rep(format(Sys.Date()-months(2),"%d/%m/%Y"),2)
-df.in$from.date=format(as.Date("2000-01-01"),"%d/%m/%Y")
-df.in$to.date=format(Sys.Date(),"%d/%m/%Y")
+df.in$to.date=rep(format(Sys.Date(),"%d/%m/%Y"),2)
 
-tase.security.daily=ddply(df.in,.(Name),.fun = function(df){
-  #set url
-    url=paste0("http://www.tase.co.il/Eng/general/company/Pages/companyHistoryData.aspx?companyID=",
-           df$companyID,
-           "&subDataType=",0,
-           "&shareID=",df$shareID)
-  #navigate to webpage
-    remDr$navigate(url)
-  #enter ui variables
-    webElem <- remDr$findElement(using = 'xpath', value = '//*[@id="ctl00_SPWebPartManager1_g_301c6a3d_c058_41d6_8169_6d26c5d97050_ctl00_HistoryData1_rbFrequency1"]')
-    webElem$setElementAttribute(attributeName = 'checked',value = 'true')
-    webElem <- remDr$findElement(using = 'xpath', value = '//*[@id="ctl00_SPWebPartManager1_g_301c6a3d_c058_41d6_8169_6d26c5d97050_ctl00_HistoryData1_rbPeriod8"]')
-    webElem$setElementAttribute(attributeName = 'checked',value = 'true')
-    remDr$executeScript(paste0("$('#ctl00_SPWebPartManager1_g_301c6a3d_c058_41d6_8169_6d26c5d97050_ctl00_HistoryData1_dailyFromCalendar_TaseCalendar_dateInput_TextBox').val('",df$from.date,"');"), args = list())
-    remDr$executeScript(paste0("$('#ctl00_SPWebPartManager1_g_301c6a3d_c058_41d6_8169_6d26c5d97050_ctl00_HistoryData1_dailyToCalendar_TaseCalendar_dateInput_TextBox').val('",df$to.date,"');"), args = list())
-  #click button
-    remDr$executeScript("$('#trhistory0').find(':button').click();", args = list())
-  #wait for data to load
-    Sys.sleep(5)
-  #import html table to parse
-    webElem <- remDr$findElement(using='xpath',value = '//*[@id="ctl00_SPWebPartManager1_g_301c6a3d_c058_41d6_8169_6d26c5d97050_ctl00_HistoryData1_gridHistoryData_DataGrid1"]')
-    out=htmlParse(remDr$getPageSource(),asText = T)
-    dataNode=getNodeSet(out,"//table[contains(@id,'gridHistoryData_DataGrid1')]")
-    metaNode =getNodeSet(out,("//table[contains(@id,'securityGrid_DataGrid1')]"))
-  #parse table into data.frame
-    tase.out=left_join(
-                readHTMLTable(metaNode[[1]],header = T)[1,c(1:4)]%>%mutate(x=1),
-                readHTMLTable(dataNode[[1]],header = T)%>%
-                    mutate_each(funs(as.Date(.,"%d/%m/%Y")),contains("date"))%>%
-                    mutate_each(funs(as.numeric(gsub("[,|%]","",.))),
-                                -contains("date"),-ends_with("type"))%>%mutate(x=1),
-                by="x")%>%
-              select(-x)
-  return(tase.out)},
-.progress = "text")
+  tase.security.daily=ddply(df.in,.(Name),.fun = function(df){
+    #set url
+      url=paste0("http://www.tase.co.il/Eng/general/company/Pages/companyHistoryData.aspx?companyID=",
+             df$companyID,
+             "&subDataType=",0,
+             "&shareID=",df$shareID)
+    #navigate to webpage
+      remDr$navigate(url)
+    #enter ui variables
+      webElem <- remDr$findElement(using = 'xpath', value = '//*[@id="ctl00_SPWebPartManager1_g_301c6a3d_c058_41d6_8169_6d26c5d97050_ctl00_HistoryData1_rbFrequency1"]')
+      webElem$setElementAttribute(attributeName = 'checked',value = 'true')
+      webElem <- remDr$findElement(using = 'xpath', value = '//*[@id="ctl00_SPWebPartManager1_g_301c6a3d_c058_41d6_8169_6d26c5d97050_ctl00_HistoryData1_rbPeriod8"]')
+      webElem$setElementAttribute(attributeName = 'checked',value = 'true')
+      remDr$executeScript(paste0("$('#ctl00_SPWebPartManager1_g_301c6a3d_c058_41d6_8169_6d26c5d97050_ctl00_HistoryData1_dailyFromCalendar_TaseCalendar_dateInput_TextBox').val('",df$from.date,"');"), args = list())
+      remDr$executeScript(paste0("$('#ctl00_SPWebPartManager1_g_301c6a3d_c058_41d6_8169_6d26c5d97050_ctl00_HistoryData1_dailyToCalendar_TaseCalendar_dateInput_TextBox').val('",df$to.date,"');"), args = list())
+    #click button
+      remDr$executeScript("$('#trhistory0').find(':button').click();", args = list())
+    #wait for data to load
+      Sys.sleep(5)
+    #import html table to parse
+      webElem <- remDr$findElement(using='xpath',value = '//*[@id="ctl00_SPWebPartManager1_g_301c6a3d_c058_41d6_8169_6d26c5d97050_ctl00_HistoryData1_gridHistoryData_DataGrid1"]')
+      out=htmlParse(remDr$getPageSource(),asText = T)
+      dataNode=getNodeSet(out,"//table[contains(@id,'gridHistoryData_DataGrid1')]")
+      metaNode =getNodeSet(out,("//table[contains(@id,'securityGrid_DataGrid1')]"))
+    #parse table into data.frame
+      tase.out=left_join(
+                  readHTMLTable(metaNode[[1]],header = T)[1,c(1:4)]%>%mutate(x=1),
+                  readHTMLTable(dataNode[[1]],header = T)%>%
+                      mutate_each(funs(as.Date(.,"%d/%m/%Y")),contains("date"))%>%
+                      mutate_each(funs(as.numeric(gsub("[,|%]","",.))),
+                                  -contains("date"),-ends_with("type"))%>%mutate(x=1),
+                  by="x")%>%
+                select(-x)
+    return(tase.out)},
+  .progress = "text")
 
 tase.security.intraday=ddply(df.in,.(Name),.fun = function(df){
     #set url
